5f2558a61003023012863fe8649be648e49c1874
Eleirold Asuncion <easuncion@cambridge.org>
2019-05-23 16:03:16 +0800

    change db params

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'ge_rnr'),
-            'username' => env('DB_USERNAME', 'easuncion'),
-            'password' => env('DB_PASSWORD', 'education_gl0bal'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', 'forge'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
